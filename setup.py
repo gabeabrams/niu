1d7c7947886033b0385a7cb3175f8e64264403a3
Gabriel Abrams <gabeabrams@gmail.com>
2017-04-07 11:01:22 -0400

    Adds dependency pulp

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from distutils.core import setup
 setup(
   name = 'niu',
   packages = ['niu'],
-  versio = '0.1',
+  version = '0.2',
   description = 'A grouping and pairing library',
   author = 'Gabriel Abrams',
   author_email = 'gabeabrams@gmail.com',
-  url = '',
-  download_url = '',
+  url = 'https://github.com/gabeabrams/niu',
+  download_url = 'https://github.com/gabeabrams/niu/archive/0.1.tar.gz',
   keywords = ['grouping', 'pairing', 'matching'],
+  install_requires=[
+    'pulp'
+  ],
   classifiers = []
 )
